ec375972de576b689f0a7f75abb0da15eb63f251
levijn <levijndejager@gmail.com>
2023-05-18 12:26:52 +0200

    changed nginx config

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const {VersionUtil} = require('@remla23-team01/lib')
 
 console.log(VersionUtil.getVersion())
 
+const env_vars = require('./env.json');
+Object.keys(env_vars).forEach(function(k) {
+    process.env[k] = env_vars[k];
+});
+
 window.addEventListener('load', function () {
+    console.log(process.env.MY_APP_URL);
 
     document.getElementById('predictSentimentBtn').addEventListener('click', function () {
         const text = document.getElementById('predictSentimentText').value;
 
-        fetch('/api', {
+        fetch(process.env.MY_APP_URL, {
             method: "POST",
             body: JSON.stringify({
                 msg: text,
